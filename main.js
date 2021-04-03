4e4cd9b1923e6e0ed0500023026cd7e726684a5f
SergeantJohnson <72981182+SergeantJohnson@users.noreply.github.com>
2021-04-03 16:50:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+webcam.set({
+    width:350,
+    height:300,
+    image_format:png,
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+webcam.attach('#camera');
+
+function takeSnapshot() 
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"</img>';
+    })
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lDjF9gmJo/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function check() {
+    img=document.getElementById("result");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    } else {
+        document.getElementById("name").innerHTML=results[0].label
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
